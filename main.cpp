d929b15ee88e02bae7ccfbd21f2a28176f0a1ee6
Cpt-Dingus <100243410+Cpt-Dingus@users.noreply.github.com>
2025-12-14 20:07:54 +0100

    Actually reads as 16-bit now.. Oops

--- main.cpp
+++ main.cpp
@@ -244,9 +244,9 @@ int main(int argc, char** argv) {
     // VIS1
     std::vector<cv::Mat> VIS1_channels;
 
-    cv::Mat VIS1_ch1 = cv::imread(RDASDirectory / "MSUGS_VIS1" / "MSUGS-VIS-1.png", cv::IMREAD_GRAYSCALE); // R
-    cv::Mat VIS1_ch2 = cv::imread(RDASDirectory / "MSUGS_VIS1" / "MSUGS-VIS-2.png", cv::IMREAD_GRAYSCALE); // G
-    cv::Mat VIS1_ch3 = cv::imread(RDASDirectory / "MSUGS_VIS1" / "MSUGS-VIS-3.png", cv::IMREAD_GRAYSCALE); // B
+    cv::Mat VIS1_ch1 = cv::imread(RDASDirectory / "MSUGS_VIS1" / "MSUGS-VIS-1.png", cv::IMREAD_UNCHANGED); // R
+    cv::Mat VIS1_ch2 = cv::imread(RDASDirectory / "MSUGS_VIS1" / "MSUGS-VIS-2.png", cv::IMREAD_UNCHANGED); // G
+    cv::Mat VIS1_ch3 = cv::imread(RDASDirectory / "MSUGS_VIS1" / "MSUGS-VIS-3.png", cv::IMREAD_UNCHANGED); // B
 
     VIS1_channels.push_back(VIS1_ch1);
     VIS1_channels.push_back(VIS1_ch2);
@@ -257,9 +257,9 @@ int main(int argc, char** argv) {
     // VIS2
     std::vector<cv::Mat> VIS2_channels;
 
-    cv::Mat VIS2_ch1 = cv::imread(RDASDirectory / "MSUGS_VIS2" / "MSUGS-VIS-1.png", cv::IMREAD_GRAYSCALE); // R
-    cv::Mat VIS2_ch2 = cv::imread(RDASDirectory / "MSUGS_VIS2" / "MSUGS-VIS-2.png", cv::IMREAD_GRAYSCALE); // G
-    cv::Mat VIS2_ch3 = cv::imread(RDASDirectory / "MSUGS_VIS2" / "MSUGS-VIS-3.png", cv::IMREAD_GRAYSCALE); // B
+    cv::Mat VIS2_ch1 = cv::imread(RDASDirectory / "MSUGS_VIS2" / "MSUGS-VIS-1.png", cv::IMREAD_UNCHANGED); // R
+    cv::Mat VIS2_ch2 = cv::imread(RDASDirectory / "MSUGS_VIS2" / "MSUGS-VIS-2.png", cv::IMREAD_UNCHANGED); // G
+    cv::Mat VIS2_ch3 = cv::imread(RDASDirectory / "MSUGS_VIS2" / "MSUGS-VIS-3.png", cv::IMREAD_UNCHANGED); // B
 
     VIS2_channels.push_back(VIS2_ch1);
     VIS2_channels.push_back(VIS2_ch2);
@@ -288,9 +288,9 @@ int main(int argc, char** argv) {
   std::vector<cv::Mat> VIS1_channels;
   std::vector<cv::UMat> VIS1_alignedChannels;
 
-  cv::Mat VIS1_ch1 = cv::imread(VIS1_directory / "MSUGS-VIS-1.png", cv::IMREAD_GRAYSCALE); // R
-  cv::Mat VIS1_ch2 = cv::imread(VIS1_directory / "MSUGS-VIS-2.png", cv::IMREAD_GRAYSCALE); // G
-  cv::Mat VIS3_ch3 = cv::imread(VIS1_directory / "MSUGS-VIS-3.png", cv::IMREAD_GRAYSCALE); // B
+  cv::Mat VIS1_ch1 = cv::imread(VIS1_directory / "MSUGS-VIS-1.png", cv::IMREAD_UNCHANGED); // R
+  cv::Mat VIS1_ch2 = cv::imread(VIS1_directory / "MSUGS-VIS-2.png", cv::IMREAD_UNCHANGED); // G
+  cv::Mat VIS3_ch3 = cv::imread(VIS1_directory / "MSUGS-VIS-3.png", cv::IMREAD_UNCHANGED); // B
 
   VIS1_channels.push_back(VIS1_ch1);
   VIS1_channels.push_back(VIS1_ch2);
@@ -303,9 +303,9 @@ int main(int argc, char** argv) {
   std::vector<cv::Mat> VIS2_channels;
   std::vector<cv::UMat> VIS2_alignedChannels;
 
-  cv::Mat VIS2_ch1 = cv::imread(VIS2_directory / "MSUGS-VIS-1.png", cv::IMREAD_GRAYSCALE); // R
-  cv::Mat VIS2_ch2 = cv::imread(VIS2_directory / "MSUGS-VIS-2.png", cv::IMREAD_GRAYSCALE); // G
-  cv::Mat VIS2_ch3 = cv::imread(VIS2_directory / "MSUGS-VIS-3.png", cv::IMREAD_GRAYSCALE); // B
+  cv::Mat VIS2_ch1 = cv::imread(VIS2_directory / "MSUGS-VIS-1.png", cv::IMREAD_UNCHANGED); // R
+  cv::Mat VIS2_ch2 = cv::imread(VIS2_directory / "MSUGS-VIS-2.png", cv::IMREAD_UNCHANGED); // G
+  cv::Mat VIS2_ch3 = cv::imread(VIS2_directory / "MSUGS-VIS-3.png", cv::IMREAD_UNCHANGED); // B
 
   VIS2_channels.push_back(VIS2_ch1);
   VIS2_channels.push_back(VIS2_ch2);
@@ -397,6 +397,10 @@ int main(int argc, char** argv) {
 
   // Writes an NC if a path was supplied
   if (NCPath != "") {
+    // TODOREWORK: NC won't work with 16-bit images. Decimate to 8 bit?
+    std::cout << "Sorry, NC generation is not supported for now!" << std::endl;
+    return 0;
+
     std::cout << "Saving NC..." << std::endl;
 
     std::vector<cv::UMat> merged{mergedCH1, mergedCH2, mergedCH3};
